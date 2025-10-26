3b096542735c7e0471a363f0f26b73606663eb8f
Samuel <escobar.samuel@correounivalle.edu.co>
2025-10-25 21:46:46 -0500

    configuración de la db

---
+++ reservas-auth-services/src/main/java/com/servicio/reservas/auth/ReservasAuthServicesApplication.java
@@ -0,0 +1,13 @@
+package com.servicio.reservas.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasAuthServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservasAuthServicesApplication.class, args);
+    }
+
+}
---
+++ reservas-auth-services/src/test/java/com/servicio/reservas/auth/ReservasAuthServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio.reservas.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasAuthServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
