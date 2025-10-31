e8b7e4d4e7d929a0fcdba455dc0fd26b4903db9b
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-10-31 15:02:05 -0500

    refactor(api): cambio en la inyección del servicio de autenticación

--- reservas-auth-services/src/main/java/com/servicio/reservas/auth/infraestructure/controller/AuthController.java
+++ reservas-auth-services/src/main/java/com/servicio/reservas/auth/infraestructure/controller/AuthController.java
@@ -2,7 +2,8 @@ package com.servicio.reservas.auth.infraestructure.controller;
 
 import com.servicio.reservas.auth.application.dto.RegisterRequest;
 import com.servicio.reservas.auth.application.dto.TokenResponse;
-import com.servicio.reservas.auth.application.services.AuthService;
+import com.servicio.reservas.auth.application.ports.in.IAuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,12 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
-    private final AuthService authService;
-
-    public AuthController(AuthService authService) {
-        this.authService = authService;
-    }
+    private final IAuthService authService;
 
     @PostMapping("/register")
     public ResponseEntity<TokenResponse> register(@RequestBody RegisterRequest registerRequest) {
@@ -26,7 +24,6 @@ public class AuthController {
 
             return ResponseEntity.status(HttpStatus.CREATED).body(tokenResponse);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
 
             return ResponseEntity.badRequest().build();
         }
