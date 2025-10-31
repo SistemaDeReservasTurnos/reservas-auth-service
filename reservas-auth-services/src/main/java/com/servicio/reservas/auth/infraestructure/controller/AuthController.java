fd61c95b15748c27169fe8f3f72a42f386aadf9d
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-10-31 16:23:15 -0500

    feat(exceptions): manejo de excepciones global para validar las peticiones

--- reservas-auth-services/src/main/java/com/servicio/reservas/auth/infraestructure/controller/AuthController.java
+++ reservas-auth-services/src/main/java/com/servicio/reservas/auth/infraestructure/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.servicio.reservas.auth.infraestructure.controller;
 import com.servicio.reservas.auth.application.dto.RegisterRequest;
 import com.servicio.reservas.auth.application.dto.TokenResponse;
 import com.servicio.reservas.auth.application.ports.in.IAuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,14 +19,9 @@ public class AuthController {
     private final IAuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<TokenResponse> register(@RequestBody RegisterRequest registerRequest) {
-        try {
-            TokenResponse tokenResponse = authService.register(registerRequest);
+    public ResponseEntity<TokenResponse> register(@Valid @RequestBody RegisterRequest registerRequest) {
+        TokenResponse tokenResponse = authService.register(registerRequest);
 
-            return ResponseEntity.status(HttpStatus.CREATED).body(tokenResponse);
-        } catch (Exception e) {
-
-            return ResponseEntity.badRequest().build();
-        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(tokenResponse);
     }
 }
--- reservas-auth-services/src/main/java/com/servicio/reservas/auth/infraestructure/persistance/TokenModel.java
+++ reservas-auth-services/src/main/java/com/servicio/reservas/auth/infraestructure/persistance/TokenModel.java
@@ -1,6 +1,10 @@
 package com.servicio.reservas.auth.infraestructure.persistance;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
