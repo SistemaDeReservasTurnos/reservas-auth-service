4e9b7bec64f17f546a41dc40baf10e250ecd4d31
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-10-31 12:07:15 -0500

    feat: habilitar el discovery client y feign client en el microservicio

--- reservas-auth-services/src/main/java/com/servicio/reservas/auth/ReservasAuthServicesApplication.java
+++ reservas-auth-services/src/main/java/com/servicio/reservas/auth/ReservasAuthServicesApplication.java
@@ -2,10 +2,13 @@ package com.servicio.reservas.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class ReservasAuthServicesApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ReservasAuthServicesApplication.class, args);
     }
