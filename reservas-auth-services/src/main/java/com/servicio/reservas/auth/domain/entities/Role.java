d2ecd692d1d91429f6c5e0f5da7fbedd991bb950
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-10-31 15:06:05 -0500

    chore: cambios en la configuración de spring sobre el servidor eureka

--- reservas-auth-services/src/main/java/com/servicio/reservas/auth/domain/entities/Role.java
+++ reservas-auth-services/src/main/java/com/servicio/reservas/auth/domain/entities/Role.java
@@ -3,5 +3,20 @@ package com.servicio.reservas.auth.domain.entities;
 public enum Role {
     CLIENTE,
     EMPLEADO,
-    ADMINISTRADOR
+    ADMINISTRADOR;
+
+    public static Role fromString(String text) {
+        if (text == null) {
+            return null;
+        }
+
+        try {
+            String cleanedText = text.trim().toUpperCase();
+
+            return Role.valueOf(cleanedText);
+
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid value: " + text);
+        }
+    }
 }
