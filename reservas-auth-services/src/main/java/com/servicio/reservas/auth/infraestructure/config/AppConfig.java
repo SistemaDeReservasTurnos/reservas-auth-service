68992e8dd732b3d16f1100810674c931541276b8
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-11-01 12:53:06 -0500

    feat(config): configuración para el manejo de la autenticación de un usuario con Spring Security

--- reservas-auth-services/src/main/java/com/servicio/reservas/auth/infraestructure/config/AppConfig.java
+++ reservas-auth-services/src/main/java/com/servicio/reservas/auth/infraestructure/config/AppConfig.java
@@ -1,12 +1,36 @@
 package com.servicio.reservas.auth.infraestructure.config;
 
+import com.servicio.reservas.auth.infraestructure.users.UserClient;
+import com.servicio.reservas.auth.infraestructure.users.UserDTO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
+    @Bean
+    public UserDetailsService userDetailsService(UserClient userClient) {
+        return username -> {
+            UserDTO user = userClient.findByEmail(username);
+
+            return User.builder()
+                    .username(user.getEmail())
+                    .password(user.getPassword())
+                    .build();
+        };
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
