6dc3c12c3d20c55571edfa8ec7f8f94c55a20cef
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-11-02 12:26:57 -0500

    feat(exception): nueva excepcion para el manejo de errores del refresh token

--- reservas-auth-services/src/main/java/com/servicio/reservas/auth/infraestructure/exception/GlobalExceptionHandler.java
+++ reservas-auth-services/src/main/java/com/servicio/reservas/auth/infraestructure/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.servicio.reservas.auth.infraestructure.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -29,9 +30,9 @@ public class GlobalExceptionHandler {
         return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Bad Request", request.getRequestURI(), fieldErrors);
     }
 
-    @ExceptionHandler(AuthenticationException.class)
+    @ExceptionHandler({AuthenticationException.class, BadCredentialsException.class})
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public ErrorResponse handleUsernameNotFoundException(AuthenticationException ex, HttpServletRequest request) {
-        return new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), ex.getMessage(), request.getRequestURI(), null);
+        return new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), ex.getLocalizedMessage(), request.getRequestURI(), null);
     }
 }
