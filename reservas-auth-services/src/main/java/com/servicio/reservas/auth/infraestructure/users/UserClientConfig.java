a7df4e734839ebd033090cd145ca5ece6426514f
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-11-16 01:38:05 -0500

    feat(feign): configuración para añadir el token para la comunicación con usuarios-service

--- reservas-auth-services/src/main/java/com/servicio/reservas/auth/infraestructure/users/UserClientConfig.java
+++ reservas-auth-services/src/main/java/com/servicio/reservas/auth/infraestructure/users/UserClientConfig.java
@@ -1,13 +1,57 @@
 package com.servicio.reservas.auth.infraestructure.users;
 
+import feign.RequestInterceptor;
 import feign.codec.ErrorDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+
+import java.util.Objects;
 
 @Configuration
 public class UserClientConfig {
     @Bean
     public ErrorDecoder errorDecoder() {
         return new MyCustomErrorDecoder();
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        OAuth2AuthorizedClientProvider authorizedClientProvider =
+                OAuth2AuthorizedClientProviderBuilder.builder()
+                        .clientCredentials()
+                        .build();
+
+        DefaultOAuth2AuthorizedClientManager authorizedClientManager =
+                new DefaultOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository, authorizedClientRepository);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return authorizedClientManager;
+    }
+
+    @Bean
+    public RequestInterceptor oauth2FeignRequestInterceptor(OAuth2AuthorizedClientManager authorizedClientManager) {
+        return requestTemplate -> {
+            OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest
+                    .withClientRegistrationId("user-service-client")
+                    .principal("reservas-auth-service")
+                    .build();
+
+            String accessToken = Objects.requireNonNull(authorizedClientManager.authorize(authorizeRequest))
+                    .getAccessToken()
+                    .getTokenValue();
+
+            requestTemplate.header("Authorization", "Bearer " + accessToken);
+        };
     }
 }
